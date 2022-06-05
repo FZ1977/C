20b308d3f004478ac8f86523ef1724a5809499a1
FZ1977 <101111973+FZ1977@users.noreply.github.com>
2022-06-05 11:22:57 +0200

    Add files via upload

--- InsertionSort.c
+++ InsertionSort.c
@@ -3,33 +3,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void stampa(int a[], int len);
+
 int main(){
 	int a[] = {6,5,3,1,8,7,2,4};
 	
-	int i=1, p, temp, len;
+	int i=1, p=0, temp, len, min;
 	
 	len = sizeof(a)/sizeof(int);
 	
 	printf("La dimensione del vettore e' %d\n", len);
-	
-	for(int x=0; x<len; x++){
-		printf("%d ", a[x]);
-	}
+	stampa(a, len);
 	printf("--> Vettore iniziale\n");
 	
+	if(i==1){
+		if(a[i]<a[p]){
+			temp=a[i];
+			a[i]=a[p];
+			a[p]=temp;
+		}		
+	}
+	
+	stampa(a,len);
+	printf("\n");
+	
+	i++;
+	p=i-1;
+	
 	while(i<len){
-		for(p=0; p<i; p++){
-			if(a[p]>a[i]){
-				temp = a[p];
-				a[p] = a[i];
-				a[i] = temp;
+		min=i;
+		while(p>=0){
+			if(a[min]<a[p]){
+				temp=a[p];
+				a[p]=a[min];
+				a[min]=temp;
 			}
+			p--;
+			min=p+1;
 		}
+		
 		i++;
-		for(int x=0; x<len; x++){
-			printf("%d ", a[x]);
-		}
+		p=i-1;
+		
+		stampa(a,len);
 		printf("\n");
 	}
+}
 
+
+void stampa(int a[], int len){
+	for(int i=0; i<len; i++){
+		printf("%d ", a[i]);
+	}
 }
