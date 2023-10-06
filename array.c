32a0a89b7cf7ffa313e5915890be83dc5c665ec3
FZ1977 <fabietto771@virgilio.it>
2023-10-06 22:26:34 +0200

    Aggiunti i file array.c dizionario.c

--- array.c
+++ array.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+void somma_uno(int x[], int n){
+	int i = 0;
+	
+	while (i < n ){
+		x[i] += 1;
+		i ++;
+	}
+}
 
 void main(){
-	int i=0;
-	int v[]={0,1,2,3,4,5,6,7,8,9};
-	int len=sizeof(v)/sizeof(int);
-/*
-	while(i<){
-		printf("%d ",v[i]);
-		i++;
+	int i, a[10]; /* Definito un array. */
+	
+	i = 0;
+	while (i < 10 ){
+		a[i] = i*100;
+		i ++;
 	}
-*/
-	printf("Numero di elementi nel vettore = %d\n",i);
+	
+	somma_uno(a, 10); /* Questa funzione ha lavorato proprio su x[] */
+	
+	i = 0;
+	while (i < 10 ){
+		printf("%d, ", a[i]);
+		i ++;
+	}
+	printf("\n");
 }
